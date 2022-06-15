6ff473fed346c67443b8b365a57e1bdb14243b7a
_Allan <allankipchumba76@gmail.com>
2022-06-15 18:29:43 +0300

    completed navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Navbar/navbar.css";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isMobile, setIsmobile] = useState(false);
+
+  const handleClick = () => setIsmobile(false);
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="logo">
           <h3>Logo</h3>
         </Link>
-        <ul className="nav-links">
-          <li>
-            <Link to="/">home</Link>
+        <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+          <li onClick={handleClick}>
+            <Link to="/" className="link">
+              home
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li onClick={handleClick}>
+            <Link to="/about" className="link">
+              About
+            </Link>
           </li>
-          <li>
-            <Link to="/skills">Skills</Link>
+          <li onClick={handleClick}>
+            <Link to="/contact" className="link">
+              Contact
+            </Link>
           </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
+          <li onClick={handleClick}>
+            <Link to="/signup" className="link">
+              Sign Up
+            </Link>
           </li>
         </ul>
+
+        <button
+          className="mobile-menu-icon"
+          onClick={() => setIsmobile(!isMobile)}
+        >
+          {isMobile ? (
+            <AiOutlineClose size={30} />
+          ) : (
+            <AiOutlineMenu size={30} />
+          )}
+        </button>
       </nav>
     </>
   );
